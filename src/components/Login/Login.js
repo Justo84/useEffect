6fea0aa3098fa79e9ddaf3687f85490d7deec106
Justin Egan <justin.d.egan@gmail.com>
2022-12-20 09:48:32 -0500

    cleanup in useEffect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
+
+    const timer = setTimeout(
+      setFormIsValid(
       enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    ), 500)
+
+    return () => {
+      clearTimeout(timer)
+    }
+    
   }, [enteredPassword, enteredEmail])
 
   const emailChangeHandler = (event) => {
